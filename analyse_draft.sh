27835c7ca83769c6af7d2ef6f84a40fb280dc256
tim <dr.janitor87@googlemail.com>
2026-01-16 17:56:45 +0100

    Draft 2027 Analysis

--- analyse_draft.sh
+++ analyse_draft.sh
@@ -1,1 +1,4 @@
-
+cp -rf ~/nfs/backup/tim-pi/MadZone ~/git/
+python3 scripts/generate_draft_class.py --year 2027
+python3 scripts/generate_draft_class_analytics.py --year 2027 --players MZ_players.csv --teams MZ_teams.csv --out docs/draft_class_2027.html
+python3 scripts/generate_index.py
